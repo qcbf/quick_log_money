a68b60a6bede5aaf3eacf3d39796c6382e8a5a80
qcbf-home <@>
2024-06-13 22:12:31 +0800

    首页

--- quick_money_flutter/lib/pages/home.dart
+++ quick_money_flutter/lib/pages/home.dart
@@ -11,16 +11,64 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Wrap(
-        children: [
-          const Text("label 1"),
-          TextButton(onPressed: () {}, child: const Text("Btn1")),
-          TextButton(onPressed: () {}, child: const Text("Btn2")),
-          TextButton(onPressed: () {}, child: const Text("Btn3")),
-          const Text("label 2"),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "notify",
+          style: TextStyle(fontSize: 14),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.widgets))
         ],
       ),
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Object? result = await showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: const Text("Add"),
+                  actions: [
+                    IconButton(
+                        onPressed: () => Navigator.pop(context, 123),
+                        icon: const Icon(Icons.done))
+                  ],
+                );
+              });
+          print(result);
+        },
+        mini: false,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text("user name"),
+            accountEmail: Text("email"),
+          ),
+          ListTile(
+            title: const Text("setting"),
+            leading: const Icon(Icons.settings),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => print("x"),
+          ),
+          ListTile(
+            title: const Text("setting"),
+            leading: const Icon(Icons.settings),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => print("x"),
+          ),
+          ListTile(
+            title: const Text("setting"),
+            leading: const Icon(Icons.settings),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () => print("x"),
+          ),
+        ],
+      )),
     );
   }
 }
