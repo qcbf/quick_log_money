da958ff18cdf9cfe21c6ee594d8f5ad810975262
杨其彪-HOME <qcbf@qq.com>
2024-09-09 20:50:28 +0800

    添加一个macos下面的注释

--- set project environment.bat
+++ set project environment.bat
@@ -1,3 +1,6 @@
 @echo off
 setx FLUTTER_STORAGE_BASE_URL https://mirrors.tuna.tsinghua.edu.cn/flutter
 setx PUB_HOSTED_URL https://mirrors.tuna.tsinghua.edu.cn/dart-pub
+
+@REM export FLUTTER_STORAGE_BASE_URL=https://mirrors.tuna.tsinghua.edu.cn/flutter
+@REM export PUB_HOSTED_URL=https://mirrors.tuna.tsinghua.edu.cn/dart-pub
